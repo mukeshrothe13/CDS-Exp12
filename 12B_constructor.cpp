24964b9461a31f2354453bb5b58178777cef09df
mukeshrothe13 <rothemukesh7@gmail.com>
2024-09-07 21:55:13 +0530

    12B_constructor.cpp

---
+++ 12B_constructor.cpp
@@ -0,0 +1,18 @@
+//Mukesh Rothe 23070123089  EXP12
+#include<iostream>
+using namespace std;
+class MyClass{
+    public:
+    string a;
+MyClass(){
+
+    cout<< "Enter your name: "<<endl;
+    cin>>a;
+    cout<<"Hello "<<a<< ", from Constructor!"<<endl;
+
+}
+};
+int main(){
+    MyClass obj;
+    return 0;
+}
